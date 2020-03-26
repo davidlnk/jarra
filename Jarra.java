13f90fc20f050820d3ec528e9f0b5c94813bb0ab
davidlnk <lenke@uma.es>
2020-03-26 12:05:45 +0100

    llenadesdeTostring

--- Jarra.java
+++ Jarra.java
@@ -14,3 +14,12 @@ public void metodoComunitario(){
 System.out.println("Aqui escribimos todos");
 }
 }
+public void llenaDesde(Jarra j){
+    while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
+        this.cantidad++; j.cantidad--;
+    }
+}
+
+public String toString(){
+    return ("("+this.capacidad+","+this.cantidad+")");
+}
